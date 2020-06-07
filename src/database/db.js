686cc59a3441cabc2b192339d55ba5e26b20ce4e
Symon Cristhian <contatosymon@gmail.com>
2020-06-07 19:25:13 -0400

    Database update

--- src/database/db.js
+++ src/database/db.js
@@ -2,6 +2,4 @@ const sqlite3 = require('sqlite3').verbose()
 
 const db =  new sqlite3.Database("./src/database/database.db")
 
-// db.run(`DELETE FROM places WHERE id = ?`, [6], function(err, results){}) 
-
 module.exports = db
